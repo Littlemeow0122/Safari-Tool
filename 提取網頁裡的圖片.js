5a1df0672d655b53eff0845264a8f186f8b8ed28
Littlemeow0122 <nicchen0122@gmail.com>
2026-05-01 20:11:50 +0800

    Update 提取網頁裡的圖片.js

--- 提取網頁裡的圖片.js
+++ 提取網頁裡的圖片.js
@@ -3,31 +3,21 @@ var result = [];
 var elements = document.getElementsByTagName('img');
 for (let element of elements) {
   if (element.src && element.src.indexOf(".svg") != -1) {
-    result.push({
-      type: "img",
-      src: element.src
-    });
+    result.push({ type: "img", src: element.src });
   }
 }
 
 var svgs = document.getElementsByTagName('svg');
 for (let svg of svgs) {
-  result.push({
-    type: "inline",
-    svg: svg.outerHTML
-  });
+  result.push({ type: "inline", svg: svg.outerHTML });
 }
 
 var all = document.getElementsByTagName('*');
 for (let el of all) {
   var style = window.getComputedStyle(el);
   var bg = style.backgroundImage;
-
   if (bg && bg.indexOf(".svg") != -1) {
-    result.push({
-      type: "css",
-      background: bg
-    });
+    result.push({ type: "css", background: bg });
   }
 }
 
